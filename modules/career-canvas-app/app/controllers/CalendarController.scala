4a8c9e183b13917fa7e76a155dcebbf8d0b7a773
Odelia Putterman <odeliaputterman@gmail.com>
2023-08-07 00:45:51 -0500

    add calendar view

---
+++ modules/career-canvas-app/app/controllers/CalendarController.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import authentication.AuthenticatedUserMessagesAction
+import model.Global
+import service.CalendarEventsService
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.twirl.api.Html
+
+import javax.inject._
+
+@Singleton
+class CalendarController @Inject()(
+  cc: MessagesControllerComponents,
+  authenticatedUserMessagesAction: AuthenticatedUserMessagesAction,
+  calendarEventsService: CalendarEventsService
+) extends MessagesAbstractController(cc) {
+
+  private val logger = play.api.Logger(this.getClass)
+
+  def showHomeCalendar(): Action[AnyContent] = authenticatedUserMessagesAction { request: MessagesRequest[AnyContent] =>
+    val userId = request.session.data(Global.SESSION_USER_ID)
+    val events = calendarEventsService
+      .findAllEvents(userId)
+      .map(event => (event.title, event.start, event.end))
+    val eventsHtml = new Html(Json.toJson(events.toList).toString())
+    Ok(views.html.calendar.calendar(eventsHtml))
+  }
+
+}
